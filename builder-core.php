94c17d03f64f2c6f7c873652483923b2151e29a8
asifpix <asifpix@gmail.com>
2020-05-01 22:17:48 +0600

    v1.3 release

--- builder-core.php
+++ builder-core.php
@@ -3,7 +3,7 @@
 Plugin Name: Builder Core
 Plugin URI:  http://ithemeslab.com
 Description: This plugin enables the core features to the Builder theme. You must have to install this plugin to work with the Builder WordPress theme.
-Version:     1.2
+Version:     1.3
 Author:      iThemesLab
 Author URI:  http://ithemeslab.com
 License:     GPL2
@@ -14,7 +14,7 @@ Domain Path: /languages
 
 // don't load directly
 if ( !defined( 'ABSPATH' ) ) {
-    die( '-1' );
+	die( '-1' );
 }
 
 load_plugin_textdomain( 'builder-core', false, basename( dirname( __FILE__ ) ) . '/languages' );
@@ -22,17 +22,17 @@ load_plugin_textdomain( 'builder-core', false, basename( dirname( __FILE__ ) ) .
 // import other files
 $main_file_dir = plugin_dir_path( __FILE__ ) . '/inc/metabox/';
 foreach ( glob( $main_file_dir . '*.php' ) as $main_files ) {
-    require_once $main_files;
+	require_once $main_files;
 }
 
 // import custom post types
 $cpt_dir = plugin_dir_path( __FILE__ ) . '/inc/cpt/';
 foreach ( glob( $cpt_dir . '*.php' ) as $cpt_files ) {
-    require_once $cpt_files;
+	require_once $cpt_files;
 }
 
 // import vc addons
 $vc_addons_dir = plugin_dir_path( __FILE__ ) . 'inc/vc-addons/';
 foreach ( glob( $vc_addons_dir . '*.php' ) as $vc_addons_files ) {
-    require_once $vc_addons_files;
+	require_once $vc_addons_files;
 }
